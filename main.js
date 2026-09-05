483eadc523df1f23e7f4b15efd6570332b29fa2e
mazen626 <mazeng810@gmail.com>
2026-09-05 23:41:28 +0300

    Initialize Electron app with main window setup

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 480,
+    height: 640,
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
